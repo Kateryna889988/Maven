08a338939bdd1ca7e66be641f3c894ebb98e87d1
Kateryna889988 <105716306+Kateryna889988@users.noreply.github.com>
2022-05-31 00:47:12 +0300

    Lesson 1 (#4)
     Fix suggestions Lesson1

--- Lecture1/src/main/java/andersen/lesson1/HomeWorkApp.java
+++ Lecture1/src/main/java/andersen/lesson1/HomeWorkApp.java
@@ -33,9 +33,9 @@ public class HomeWorkApp {
     public static void printColor(int value) {
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
-        } else if (value >= 100) {
+        } else {
             System.out.println("Зеленый");
         }
     }
